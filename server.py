a1c84bbd7ee589f63ccfd55e31c8ce8f92268238
Tina Zheng <tinazheng@berkeley.edu>
2014-09-24 19:54:23 -0700

    Added question 1 and question 2

--- server.py
+++ server.py
@@ -5,9 +5,28 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
-@app.route('/quiz')
-def game():
-	return render_template("game.html")
+def question(number):
+	if number == 1:
+		return "What is the best programming langauge?"
+	if number == 2:
+		return "Fill in the blank: John _____"
+
+@app.route('/quiz/<int:num>')
+def game(num):
+	s = question(num)
+	return render_template("game.html", question=s, level=num)
+
+@app.route('/quiz/<int:num>', methods=['POST'])
+def check(num):
+	answer = request.form['text']
+	if num == 1:
+		if answer == "Python":
+			return redirect('/quiz/'+str(num+1))
+	if num == 2:
+		if answer == "DeNero":
+			return redirect('/quiz/' + str(num-1))
+		else:
+			return "Wrong"
 
 if __name__ == '__main__':
     app.run(debug=True)
